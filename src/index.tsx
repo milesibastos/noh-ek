3034e1c9bc9331a8e467c91b309bbc6c90238b3f
@milesibastos <antonio@milesibastos.com>
2022-01-29 18:37:08 -0300

    yarn add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBPO_UsBMQlISSVgn7C2s7-og1qWq9edjE',
+  authDomain: 'noh-ek.firebaseapp.com',
+  projectId: 'noh-ek',
+  storageBucket: 'noh-ek.appspot.com',
+  messagingSenderId: '291346563524',
+  appId: '1:291346563524:web:68f499d01bd9188667a491',
+  measurementId: 'G-KFSP6LD11N',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.debug(analytics);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,4 +37,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
